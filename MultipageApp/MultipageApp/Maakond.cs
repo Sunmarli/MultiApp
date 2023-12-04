9d2a740363aa4c12fee36033ab6149cad75176a7
Sunmarli <118188337+Sunmarli@users.noreply.github.com>
2023-12-04 11:59:37 +0200

    Add project files.

---
+++ MultipageApp/MultipageApp/Maakond.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MultipageApp
+{
+    public class Maakond : INotifyPropertyChanged
+    {
+        string nimetus;
+        string pealinn;
+        int inimeste_arv;
+        int pindala;
+        string asukoht;
+        public Maakond() { }
+        public Maakond(string Nimetus, string Pealinn, int Inimeste_arv, int Pindala,string Asukoht)
+        {
+            nimetus = Nimetus;
+            pealinn = Pealinn;
+            inimeste_arv = Inimeste_arv;
+            pindala = Pindala;
+            asukoht = Asukoht;
+        }
+        public string Nimetus
+        {
+            get { return nimetus; }
+            set
+            {
+                if (nimetus != value)
+                {
+                    nimetus = value;
+                    OnPropertyChanged("Nimetus");
+
+                }
+            }
+
+        }
+        public string Pealinn
+        {
+            get { return pealinn; }
+            set
+            {
+                if (pealinn != value)
+                {
+                    pealinn = value;
+                    OnPropertyChanged("Pealinn");
+
+                }
+            }
+
+        }
+        public int Inimeste_arv
+        {
+            get { return inimeste_arv; }
+            set
+            {
+                if (inimeste_arv != value)
+                {
+                    inimeste_arv = value;
+                    OnPropertyChanged("Inimeste_arv");
+
+                }
+            }
+
+        }
+        public int Pindala
+        {
+            get { return pindala; }
+            set
+            {
+                if (pindala != value)
+                {
+                    pindala = value;
+                    OnPropertyChanged("pindala");
+
+                }
+            }
+
+        }
+
+        public string Asukoht
+        {
+            get { return asukoht; }
+            set
+            {
+                if (asukoht != value)
+                {
+                    asukoht = value;
+                    OnPropertyChanged("asukoht");
+
+                }
+            }
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string v = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+    }
+}
---
+++ MultipageApp/MultipageApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MultipageApp
+{
+    public partial class MainPage : ContentPage
+    {
+        protected internal ObservableCollection<Maakond> Maakonnads { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Maakonnads = new ObservableCollection<Maakond>
+            {
+                new Maakond {Nimetus="Harjumaa",Pealinn="Tallinn", Inimeste_arv=52000},
+                new Maakond {Nimetus="Tartumaa",Pealinn="Tartu", Inimeste_arv=30000},
+                new Maakond {Nimetus="Ida-Virumaa",Pealinn="Kohtla-Järve", Inimeste_arv=10000},
+            };
+            //blalblala
+            list.BindingContext = Maakonnads;
+
+            list.BindingContext = Maakonnads;
+            list.BindingContext = Maakonnads;
+            list.BindingContext = Maakonnads;
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Maakonnad(null));
+        }
+
+        private async void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Maakond selected = e.SelectedItem as Maakond;
+            if (selected != null)
+            {
+                list.SelectedItem = null;
+                await Navigation.PushAsync(new Maakonnad(selected));
+            }
+
+        }
+        protected internal void AddMaakond(Maakond maakond)
+        {
+            Maakonnads.Add(maakond);
+        }
+    }
+}
